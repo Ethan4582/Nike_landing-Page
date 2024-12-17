9990a855c551be1238f2ce82efa11937e9bcb106
ASH <singhashirwad2003@gmail.com>
2024-12-18 02:16:03 +0530

    first Commit

---
+++ src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from './Hero'
+import Footer from './Footer'
+import PopularProduct from './PopularProduct'
+import CustomerReview from './CusomterReviews'
+import SpecialOffer from './SpecialOffer'
+import SuperQuality from './SuperQuality'
+import  Services from './Services'
+import  Subscribe from './Subscribe'
+export {
+   Hero,
+   CustomerReview,
+   Footer,
+   PopularProduct,
+   Services,
+   SpecialOffer,
+   Subscribe,
+   SuperQuality
+}
